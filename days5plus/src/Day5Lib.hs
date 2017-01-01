5cebb32e101215a1a37bfa2b26787e931e10b93f
Quinn Wilson <git@midword.org>
2017-01-01 15:21:04 -0600

    Day5 a/b complete

--- days5plus/src/Day5Lib.hs
+++ days5plus/src/Day5Lib.hs
@@ -3,11 +3,14 @@ module Day5Lib
       ,generatePassword
       ,findFiveZeroHash
       ,getSixthDigit
+      ,getSixthAndSeventhDigit
+      ,day5Decrypt
     ) where
 
 import qualified Data.ByteString
 import qualified Data.ByteString.Char8 as Char8
 import qualified Data.Char
+import qualified Data.List as List
 import qualified Data.ByteString.Base16 as Base16
 import qualified Crypto.Hash.MD5 as MD5
 
@@ -46,4 +49,16 @@ generatePassword pass = do
 
 getSixthDigit :: String -> Char
 getSixthDigit pass = (getHash pass)!!5
+
+getSixthAndSeventhDigit :: String -> (Char, Char)
+getSixthAndSeventhDigit pass = do
+  let hash = (getHash pass) 
+  (hash!!5, hash!!6)
    
+day5Decrypt :: String -> String
+day5Decrypt input = 
+      map snd $ List.sort $ take 8 $ List.nubBy (\y x-> (fst y) == (fst x)) $ 
+      filter (\y -> elem (fst y) ['0'..'7']) $ 
+      map (\y -> getSixthAndSeventhDigit (input ++ show y)) $ 
+      generatePassword  input
+  
--- days5plus/test/Spec.hs
+++ days5plus/test/Spec.hs
@@ -1,6 +1,7 @@
 import Test.Hspec
 import Test.QuickCheck
 import Day5Lib
+import qualified Data.List
 
 main :: IO ()
 main = hspec $ do
@@ -30,4 +31,24 @@ main = hspec $ do
         let output = "f77a0e6e"
         --let col = findFiveZeroHash input 8
         --map (\y -> getSixthDigit (input ++ show y)) col `shouldBe` output
+        pendingWith "takes too long to run, uncomment if neccesary"
+
+    describe "getSixthAndSeventhDigit" $ do 
+      it "should find the 6th and 7th character in a hash from the key" $ do
+        let input = "abc3231929"
+        let output = ('1', '5')
+        getSixthAndSeventhDigit input `shouldBe` output
+
+
+    describe "runs all 8 and gets the password to part b" $ do 
+      it "should run give the answer to part b" $ do
+        let input = "cxdnnyjw"
+        let output = "999828ec"
+--        day5Decrypt input `shouldBe` output
+        pendingWith "takes too long to run, uncomment if neccesary"
+
+      it "should run give the unsorted answer to part b" $ do
+        let input = "abc"
+        let output = "05ace8e3"
+--        day5Decrypt input `shouldBe` output
         pendingWith "takes too long to run, uncomment if neccesary"
